5f292ef494c4ee35ad3c662da3397236efda9eca
gukx12345 <kevinthedoer@gmail.com>
2026-03-21 14:53:54 -0400

    added introduction to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Kevin" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print("Hello everyone! My name is Kevin Gu, and I'm a sophomore from West Chester Pennsylvania. Some of my favorite school subjects are History and Math. I enjoy doing robotics, and I take part in the First Robotics Competition in FRC Team 365 and FTC Team 395. I also like military aviation (my favorite plane is the MIG-21bis).I'm excited to learn about aerial robotics in this class, as I really enjoy aviation and robotics, and would like to see how those two subjects combine together to make something amazing!")
 if __name__ == "__main__":
     main()
